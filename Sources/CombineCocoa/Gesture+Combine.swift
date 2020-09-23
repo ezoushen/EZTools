12cffcb0b6ad882c07272279966108baca247e9e
ezou <ezoushen@gmail.com>
2020-09-23 14:11:54 +0800

    - update gesture recognizer

--- Sources/CombineCocoa/Gesture+Combine.swift
+++ Sources/CombineCocoa/Gesture+Combine.swift
@@ -47,14 +47,15 @@ public protocol GestureProtocol {
 public protocol Gesture: GestureProtocol {
     associatedtype GestureRecognizer: UIGestureRecognizer
     var recognizer: GestureRecognizer { get }
+    var delegate: GestureRecognizerDelegate { get }
 }
 
 public extension Gesture {
-    typealias Configuration = (GestureRecognizer) -> Void
+    typealias Configuration = (GestureRecognizerDelegate, GestureRecognizer) -> Void
     
-    init(_ block: (GestureRecognizer) -> Void) {
+    init(_ block: Configuration) {
         self = Self.init()
-        block(recognizer)
+        block(delegate, recognizer)
     }
 }
 
@@ -71,17 +72,23 @@ public struct AnyGesture: Gesture {
     
     public let recognizer: UIGestureRecognizer
     
+    public let delegate: GestureRecognizerDelegate
+    
     public init() {
         recognizer = .init()
+        delegate = GestureRecognizerDelegateProxy()
     }
     
     init<G: Gesture>(_ gesture: G) {
         self.recognizer = gesture.recognizer
+        self.delegate = gesture.delegate
     }
 }
 
 public struct TapGesture: Gesture {
     public let recognizer = UITapGestureRecognizer()
+    
+    public let delegate: GestureRecognizerDelegate = GestureRecognizerDelegateProxy()
     
     public init() { }
 }
@@ -89,11 +96,15 @@ public struct TapGesture: Gesture {
 public struct SwipeGesture: Gesture {
     public let recognizer = UISwipeGestureRecognizer()
     
+    public let delegate: GestureRecognizerDelegate = GestureRecognizerDelegateProxy()
+    
     public init() { }
 }
 
 public struct LongPressGesture: Gesture {
     public let recognizer = UILongPressGestureRecognizer()
+    
+    public let delegate: GestureRecognizerDelegate = GestureRecognizerDelegateProxy()
     
     public init() { }
 }
@@ -101,11 +112,15 @@ public struct LongPressGesture: Gesture {
 public struct PanGesture: Gesture {
     public let recognizer = UIPanGestureRecognizer()
     
+    public let delegate: GestureRecognizerDelegate = GestureRecognizerDelegateProxy()
+    
     public init() { }
 }
 
 public struct PinchGesture: Gesture {
     public let recognizer = UIPinchGestureRecognizer()
+    
+    public let delegate: GestureRecognizerDelegate = GestureRecognizerDelegateProxy()
     
     public init() { }
 }
@@ -113,23 +128,27 @@ public struct PinchGesture: Gesture {
 public struct EdgeGesture: Gesture {
     public let recognizer = UIScreenEdgePanGestureRecognizer()
     
+    public let delegate: GestureRecognizerDelegate = GestureRecognizerDelegateProxy()
+    
     public init() { }
 }
 
 public struct ForceTouchGesture: Gesture {
     public let recognizer = UIForceTouchGestureRecognizer()
     
+    public let delegate: GestureRecognizerDelegate = GestureRecognizerDelegateProxy()
+    
     public init() { }
 }
 
 public enum GestureType {
-    case tap((UITapGestureRecognizer) -> Void = { _ in })
-    case pinch((UIPinchGestureRecognizer) -> Void = { _ in })
-    case longPress((UILongPressGestureRecognizer) -> Void = { _ in })
-    case edge((UIScreenEdgePanGestureRecognizer) -> Void = { _ in })
-    case swipe((UISwipeGestureRecognizer) -> Void = { _ in })
-    case pan((UIPanGestureRecognizer) -> Void = { _ in })
-    case forceTouch((UIForceTouchGestureRecognizer) -> Void = { _ in })
+    case tap((GestureRecognizerDelegate, UITapGestureRecognizer) -> Void = { _, _ in })
+    case pinch((GestureRecognizerDelegate, UIPinchGestureRecognizer) -> Void = { _, _ in })
+    case longPress((GestureRecognizerDelegate, UILongPressGestureRecognizer) -> Void = { _, _ in })
+    case edge((GestureRecognizerDelegate, UIScreenEdgePanGestureRecognizer) -> Void = { _, _ in })
+    case swipe((GestureRecognizerDelegate, UISwipeGestureRecognizer) -> Void = { _, _ in })
+    case pan((GestureRecognizerDelegate, UIPanGestureRecognizer) -> Void = { _, _ in })
+    case forceTouch((GestureRecognizerDelegate, UIForceTouchGestureRecognizer) -> Void = { _, _ in })
 }
 
 public extension GestureType {
@@ -147,13 +166,13 @@ public extension GestureType {
     
     var configuration: AnyGesture.Configuration {
         switch self {
-        case .tap(let block):          return { block($0 as! UITapGestureRecognizer) }
-        case .pinch(let block):        return { block($0 as! UIPinchGestureRecognizer) }
-        case .longPress(let block):    return { block($0 as! UILongPressGestureRecognizer) }
-        case .edge(let block):         return { block($0 as! UIScreenEdgePanGestureRecognizer) }
-        case .swipe(let block):        return { block($0 as! UISwipeGestureRecognizer) }
-        case .pan(let block):          return { block($0 as! UIPanGestureRecognizer) }
-        case .forceTouch(let block):   return { block($0 as! UIForceTouchGestureRecognizer)}
+        case .tap(let block):          return { block($0, $1 as! UITapGestureRecognizer) }
+        case .pinch(let block):        return { block($0, $1 as! UIPinchGestureRecognizer) }
+        case .longPress(let block):    return { block($0, $1 as! UILongPressGestureRecognizer) }
+        case .edge(let block):         return { block($0, $1 as! UIScreenEdgePanGestureRecognizer) }
+        case .swipe(let block):        return { block($0, $1 as! UISwipeGestureRecognizer) }
+        case .pan(let block):          return { block($0, $1 as! UIPanGestureRecognizer) }
+        case .forceTouch(let block):   return { block($0, $1 as! UIForceTouchGestureRecognizer)}
         }
     }
 }
@@ -161,7 +180,7 @@ public extension GestureType {
 public final class GestureSubscription<S: Subscriber, G: Gesture>: NSObject, Subscription, UIGestureRecognizerDelegate where S.Input == G, S.Failure == Never {
     private var subscriber: S?
     private var gesture: G
-    
+        
     private weak var view: UIView?
     
     init(subscriber: S, view: UIView, gesture: G) {
@@ -175,10 +194,10 @@ public final class GestureSubscription<S: Subscriber, G: Gesture>: NSObject, Sub
     }
     
     private func configureGesture(_ gesture: G) {
-        let gesture = gesture.recognizer
-        gesture.delegate = self
-        gesture.addTarget(self, action: #selector(handler))
-        view?.addGestureRecognizer(gesture)
+        let recognizer = gesture.recognizer
+        recognizer.delegate = gesture.delegate.delegate
+        recognizer.addTarget(self, action: #selector(handler))
+        view?.addGestureRecognizer(recognizer)
     }
     
     public func request(_ demand: Subscribers.Demand) { }
@@ -196,8 +215,62 @@ public final class GestureSubscription<S: Subscriber, G: Gesture>: NSObject, Sub
         _ = subscriber?.receive(gesture)
     }
     
+}
+
+public protocol GestureRecognizerDelegate: AnyObject {
+    var delegate: UIGestureRecognizerDelegate { get }
+    var shouldBegin: (_ gestureRecognizer: UIGestureRecognizer) -> Bool { get set }
+    var shouldRequireFailureOf: (_ gestureRecognizer: UIGestureRecognizer, _ otherGestureRecognizer: UIGestureRecognizer) -> Bool { get set }
+    var shouldBeRequiredToFailBy: (_ gestureRecognizer: UIGestureRecognizer, _ shouldBeRequiredToFailBy: UIGestureRecognizer) -> Bool { get set }
+    var shouldReceiveTouch: (_ gestureRecognizer: UIGestureRecognizer, _ touch: UITouch) -> Bool { get set }
+    var shouldReceivePress: (_ gestureRecognizer: UIGestureRecognizer, _ press: UIPress) -> Bool { get set }
+    var shouldReceiveEvent: (_ gestureRecognizer: UIGestureRecognizer, _ event: UIEvent) -> Bool { get set }
+    var shouldRecognizeSimultaneouslyWith: (_ gestureRecognizer: UIGestureRecognizer, _ otherGestureRecognizer: UIGestureRecognizer) -> Bool { get set }
+}
+
+class GestureRecognizerDelegateProxy: NSObject, GestureRecognizerDelegate, UIGestureRecognizerDelegate {
+    var delegate: UIGestureRecognizerDelegate { self }
+    
+    var shouldBegin: (UIGestureRecognizer) -> Bool = { _ in true }
+    
+    var shouldRequireFailureOf: (UIGestureRecognizer, UIGestureRecognizer) -> Bool = { _, _ in false }
+    
+    var shouldBeRequiredToFailBy: (UIGestureRecognizer, UIGestureRecognizer) -> Bool = { _, _ in false }
+    
+    var shouldReceiveTouch: (UIGestureRecognizer, UITouch) -> Bool = { _, _ in true }
+    
+    var shouldReceivePress: (UIGestureRecognizer, UIPress) -> Bool = { _, _ in true }
+    
+    var shouldReceiveEvent: (UIGestureRecognizer, UIEvent) -> Bool = { _, _ in true }
+    
+    var shouldRecognizeSimultaneouslyWith: (UIGestureRecognizer, UIGestureRecognizer) -> Bool = { _, _ in true }
+    
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        shouldBegin(gestureRecognizer)
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        shouldRequireFailureOf(gestureRecognizer, otherGestureRecognizer)
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        shouldBeRequiredToFailBy(gestureRecognizer, otherGestureRecognizer)
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        shouldReceiveTouch(gestureRecognizer, touch)
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive press: UIPress) -> Bool {
+        shouldReceivePress(gestureRecognizer, press)
+    }
+    
+    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive event: UIEvent) -> Bool {
+        shouldReceiveEvent(gestureRecognizer, event)
+    }
+    
     public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        true
+        shouldRecognizeSimultaneouslyWith(gestureRecognizer, otherGestureRecognizer)
     }
 }
 
@@ -210,42 +283,42 @@ public extension CBPublishers where Base: UIView {
         publishGesture(type: AnyGesture.self, configuration: gestureType.configuration)
     }
     
-    func tapGesture(_ configuration: TapGesture.Configuration = { _ in }) -> GesturePublisher<TapGesture> {
+    func tapGesture(_ configuration: TapGesture.Configuration = { _, _ in }) -> GesturePublisher<TapGesture> {
         publishGesture(type: TapGesture.self, configuration: configuration)
     }
     
-    func panGesture(_ configuration: PanGesture.Configuration = { _ in }) -> GesturePublisher<PanGesture> {
+    func panGesture(_ configuration: PanGesture.Configuration = { _, _ in }) -> GesturePublisher<PanGesture> {
         publishGesture(type: PanGesture.self, configuration: configuration)
     }
     
-    func swipeGesture(_ configuration: SwipeGesture.Configuration = { $0.direction = [.right, .left, .up, .down] }) -> GesturePublisher<SwipeGesture> {
+    func swipeGesture(_ configuration: SwipeGesture.Configuration = { _, gesture in gesture.direction = [.right, .left, .up, .down] }) -> GesturePublisher<SwipeGesture> {
         publishGesture(type: SwipeGesture.self, configuration: configuration)
     }
     
     func swipeGesture(_ directions: UISwipeGestureRecognizer.Direction...) -> GesturePublisher<SwipeGesture> {
-        swipeGesture{ $0.direction = .init(directions) }
+        swipeGesture{ _, gesture in gesture.direction = .init(directions) }
     }
     
-    func edgeGesture(_ configuration: EdgeGesture.Configuration = { _ in }) -> GesturePublisher<EdgeGesture> {
+    func edgeGesture(_ configuration: EdgeGesture.Configuration = { _, _ in }) -> GesturePublisher<EdgeGesture> {
         publishGesture(type: EdgeGesture.self, configuration: configuration)
     }
     
     func edgeGesture(_ edges: UIRectEdge...) -> GesturePublisher<EdgeGesture> {
-        edgeGesture{ $0.edges = UIRectEdge(edges) }
+        edgeGesture{ _, gesture in gesture.edges = UIRectEdge(edges) }
     }
     
-    func longPressGesture(_ configuration: LongPressGesture.Configuration = { _ in }) -> GesturePublisher<LongPressGesture> {
+    func longPressGesture(_ configuration: LongPressGesture.Configuration = { _, _ in }) -> GesturePublisher<LongPressGesture> {
         publishGesture(type: LongPressGesture.self, configuration: configuration)
     }
     
-    func pinchGesture(_ configuration: PinchGesture.Configuration = { _ in }) -> GesturePublisher<PinchGesture> {
+    func pinchGesture(_ configuration: PinchGesture.Configuration = { _, _ in }) -> GesturePublisher<PinchGesture> {
         publishGesture(type: PinchGesture.self, configuration: configuration)
     }
     
-    func forceTouchGesture(threshold: CGFloat = 1.0, _ configuration: ForceTouchGesture.Configuration = { _ in }) -> GesturePublisher<ForceTouchGesture> {
+    func forceTouchGesture(threshold: CGFloat = 1.0, _ configuration: ForceTouchGesture.Configuration = { _, _ in }) -> GesturePublisher<ForceTouchGesture> {
         publishGesture(type: ForceTouchGesture.self, configuration: {
-            $0.threshold = threshold
-            configuration($0)
+            $1.threshold = threshold
+            configuration($0, $1)
         })
     }
 }
