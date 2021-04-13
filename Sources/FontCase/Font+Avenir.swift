57c5554818e717b69b7cf250b47ed0d6dcbf90c4
ezou <ezoushen@gmail.com>
2021-04-13 15:30:11 +0800

    - reordered

--- Sources/FontCase/Font+Avenir.swift
+++ Sources/FontCase/Font+Avenir.swift
@@ -5,12 +5,12 @@ import SwiftUI
 public struct Avenir: FontNamespace { }
 
 public extension FontSeries where Namespace == Avenir {
-    var medium: FontCustomizer<Avenir, F> { .init(name: "Medium") }
-    var roman: FontCustomizer<Avenir, F> { .init(name: "Roman") }
-    var black: FontCustomizer<Avenir, F> { .init(name: "Black") }
     var light: FontCustomizer<Avenir, F> { .init(name: "Light") }
     var book: FontCustomizer<Avenir, F> { .init(name: "Book") }
+    var roman: FontCustomizer<Avenir, F> { .init(name: "Roman") }
+    var medium: FontCustomizer<Avenir, F> { .init(name: "Medium") }
     var heavy: FontCustomizer<Avenir, F> { .init(name: "Heavy") }
+    var black: FontCustomizer<Avenir, F> { .init(name: "Black") }
 }
 
 public extension FontFamily where Namespace == Avenir {
